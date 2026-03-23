488b4bc5c6c3040f8ff5961da985b607803e1ca0
marcosrabelocod <marcosrabelocod@gmail.com>
2026-03-22 23:41:20 -0300

    adição das regras de negocio, e do controller que vai receber os comandos e opera a api

---
+++ src/main/java/desafio_pagamento/controller/PagamentoController.java
@@ -0,0 +1,76 @@
+package desafio_pagamento.controller;
+
+import desafio_pagamento.dto.PagamentoRequestDTO;
+import desafio_pagamento.model.Pagamento;
+import desafio_pagamento.service.PagamentoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pagamentos")
+public class PagamentoController {
+
+    private final PagamentoService pagamentoService;
+
+    public PagamentoController(PagamentoService pagamentoService){
+        this.pagamentoService = pagamentoService;
+    }
+
+    //1 Rota para receber um novo pagamento(Post)
+    @PostMapping
+    public ResponseEntity<?> receberPagamento(@RequestBody PagamentoRequestDTO dto){
+        try{
+            //Passa o DTO para o Service validar e guardar
+            Pagamento pagamentoSalvo = pagamentoService.receberPagamento(dto);
+            return ResponseEntity.ok(pagamentoSalvo);
+        } catch (IllegalArgumentException e){
+            //se a validação do cartão credito ou debito falahar, erro 400
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    //2 Rota para atualizar o status (Put)
+    @PutMapping("/{id}/status")
+    public ResponseEntity<?> atualizarStatus(@PathVariable Long id, @RequestBody Map<String, String> payload){
+        try{
+            String novoStatus = payload.get("status");
+            if (novoStatus == null || novoStatus.trim().isEmpty()){
+                return ResponseEntity.badRequest().body("O campo 'status' é obrigatório no JSON.");
+            }
+
+            // Tenta atualizar. Se as regras da Máquina de Estados falharem, o Service lança uma exceção.
+            pagamentoService.atualizarStatus(id, novoStatus);
+            return ResponseEntity.ok("Status do pagamento " + id + " atualizar para: " + novoStatus);
+
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            // Apanha os erros das regras de negócio (Ex: tentar alterar um sucesso) e devolve Erro 400
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    //3 Rota para a exclusão lógica (Delete)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> inativarPAgamento(@PathVariable Long id){
+        try{
+            pagamentoService.inativarPagamento(id);
+            return ResponseEntity.ok("Pagamento inativo com sucesso.");
+        } catch(IllegalArgumentException | IllegalStateException e){
+            // Apanha o erro se tentar apagar algo que já não está Pendente
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    // 4 Rota para listar pagamentos com os filtros dinâmicos (GET)
+    @GetMapping
+    public ResponseEntity<List<Pagamento>> ListaPagamento(
+        @RequestParam(required = false) Integer codigoDebito,
+        @RequestParam(required = false) String cpfCnpj,
+        @RequestParam(required = false) String status) {
+        // O @RequestParam permite que a URL receba parâmetros como: 
+        // /pagamentos?status=Pendente de Processamento&codigoDebito=123
+            List<Pagamento> pagamentos = pagamentoService.listaPagamento(codigoDebito, cpfCnpj, status);
+            return ResponseEntity.ok(pagamentos);
+        }
+}
---
+++ src/main/java/desafio_pagamento/service/PagamentoService.java
@@ -0,0 +1,84 @@
+package desafio_pagamento.service;
+
+import desafio_pagamento.dto.PagamentoRequestDTO;
+import desafio_pagamento.model.Pagamento;
+import desafio_pagamento.repository.PagamentoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PagamentoService {
+    public final PagamentoRepository pagamentoRepository;
+
+    public PagamentoService(PagamentoRepository pagamentoRepository){
+        this.pagamentoRepository = pagamentoRepository;
+    }
+
+    //1 Receber e validar novo pagamento
+    public Pagamento receberPagamento(PagamentoRequestDTO dto){
+        //Validação de numero do cartão somente para as opções credito ou debito
+        String metodo = dto.getMetodoPagamento();
+        if ((metodo.equals("cartao_credito") || metodo.equals("cartao_debito"))&&
+            (dto.getNumeroCartao() == null || dto.getNumeroCartao().trim().isEmpty())){
+            throw new IllegalArgumentException("Numero do cartão é obrigatorio para pagamento com cartão");
+        }
+
+        // Converte o DTO para a entidade (Model)
+        Pagamento pagamento = new Pagamento();
+        pagamento.setCodigoDebito(dto.getCodigoDebito());
+        pagamento.setCpfCnpj(dto.getCpfCnpj());
+        pagamento.setMetodoPagamento(dto.getMetodoPagamento());
+        pagamento.setNumeroCartao(dto.getNumeroCartao());
+        pagamento.setValor(dto.getValor());
+
+        //o status "Pendente = True" já é colocadono construtor do pagamento
+
+        return pagamentoRepository.salvar(pagamento);
+    }
+
+    //2 Função que atualiza os Status (Maquina de de Estados)
+    public void atualizarStatus(Long id, String novoStatus) {
+        Pagamento pagamento = pagamentoRepository.buscarPorId(id).orElseThrow(() -> new IllegalArgumentException("Pagamento não encontrado ou inativo"));
+
+        String statusAtual = pagamento.getStatus();
+
+        //Regra C: Quando Processado com sucesso, não pode ser alterado.
+        if (statusAtual.equals("Processado com Sucesso")){
+            throw new IllegalStateException("Um pagamento já processado com sucesso não pode ter o seu status alterado.");
+        }
+
+        // Regra D: Quando Processado com Falha, SÓ PODE ir para Pendente de Processamento.
+        if (statusAtual.equals("Processado com Falha")){
+            if(!novoStatus.equals("Pendente de Processamento")){
+                throw new IllegalStateException("Pagamentos com falha apenas podem voltar para 'Pendente de Processamento'.");
+            }
+        }
+
+        // Regra B: Quando Pendente de Processamento, pode ir para Sucesso ou Falha.
+        if (statusAtual.equals("Pendente de Processamento")){
+            if(!novoStatus.equals("Processado com sSucesso") && !novoStatus.equals("Processado com Falha")) {
+                throw new IllegalStateException("Status inválido. De Pendente, apenas pode ir para Sucesso ou Falha.");    
+            }
+        }
+
+        // Se passou por todas as barreiras de segurança, atualizamos no banco
+        pagamentoRepository.atualizarStatus(id, novoStatus);
+    }
+
+    // 3. Exclusão Lógica (Inativar)
+    public void inativarPagamento(Long id){
+        Pagamento pagamento = pagamentoRepository.buscarPorId(id).orElseThrow(() -> new IllegalArgumentException("Pagamento não encontrado."));
+    
+        // Regra 4: A exclusão lógica apenas pode ocorrer se estiver Pendente de Processamento
+        if (!pagamento.getStatus().equals("Pendente de Processamento")){
+            throw new IllegalStateException("Apenas pagamento com status 'Pendente de Processamento' podem ser excuidos.");
+        }
+        pagamentoRepository.inativar(id);
+    }
+
+    //4 Lista com Filtros
+    public List<Pagamento> listaPagamento(Integer codigoDebito, String cpfCnpj, String status){
+        return pagamentoRepository.listaComFiltros(codigoDebito, cpfCnpj, status);
+    }
+}
